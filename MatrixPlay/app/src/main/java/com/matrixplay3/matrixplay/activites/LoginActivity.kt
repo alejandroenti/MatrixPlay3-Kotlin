020e977f808cadb38bee9c687952f98eca48e4e5
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-11-07 16:52:43 +0100

    Adding Logging Activity

---
+++ MatrixPlay/app/src/main/java/com/matrixplay3/matrixplay/activites/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.matrixplay3.matrixplay.activites
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.matrixplay3.matrixplay.R
+
+class LoginActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
