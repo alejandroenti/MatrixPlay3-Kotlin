c9ac3fd24b67e53e7fd5ff406ffc6c42f9b34158
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-11-07 19:10:07 +0100

    Getting data from inputs
    Enabling and disabling the button to connect depending on the input text

--- MatrixPlay/app/src/main/java/com/matrixplay3/matrixplay/activites/LoginActivity.kt
+++ MatrixPlay/app/src/main/java/com/matrixplay3/matrixplay/activites/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.matrixplay3.matrixplay.activites
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
 import com.matrixplay3.matrixplay.R
 
 class LoginActivity : BaseActivity() {
+
+    private lateinit var username : TextInputEditText
+    private lateinit var serverUrl : TextInputEditText
+    private lateinit var btnConnect : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,29 @@ class LoginActivity : BaseActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        username = findViewById<TextInputEditText>(R.id.loginUsername)
+        serverUrl = findViewById<TextInputEditText>(R.id.loginURL)
+        btnConnect = findViewById<Button>(R.id.loginBtnConnect)
+
+        username.addTextChangedListener {
+            enableBtn(areFieldsFilled())
+        }
+
+        serverUrl.addTextChangedListener {
+            enableBtn(areFieldsFilled())
+        }
+
+        btnConnect.setOnClickListener {
+            // Connect To Server
+        }
+    }
+
+    private fun areFieldsFilled() : Boolean {
+        return !username.text.isNullOrBlank() && !serverUrl.text.isNullOrBlank()
+    }
+
+    private fun enableBtn(status : Boolean) {
+        btnConnect.isEnabled = status
     }
 }
