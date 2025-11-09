9079a29cfef58b9384cdc39c7c688c23a817af09
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-11-09 22:31:46 +0100

    Adding logo to Login View

--- MatrixPlay/app/src/main/java/com/matrixplay3/matrixplay/activites/LoginActivity.kt
+++ MatrixPlay/app/src/main/java/com/matrixplay3/matrixplay/activites/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.matrixplay3.matrixplay.activites
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
@@ -17,6 +19,7 @@ class LoginActivity : BaseActivity() {
     private lateinit var username : TextInputEditText
     private lateinit var serverUrl : TextInputEditText
     private lateinit var btnConnect : Button
+    private lateinit var logo : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +34,10 @@ class LoginActivity : BaseActivity() {
         username = findViewById<TextInputEditText>(R.id.loginUsername)
         serverUrl = findViewById<TextInputEditText>(R.id.loginURL)
         btnConnect = findViewById<Button>(R.id.loginBtnConnect)
+        logo = findViewById<ImageView>(R.id.loginLogo)
+
+        val bitmap = BitmapFactory.decodeStream( assets.open("logo.png") )
+        logo.setImageBitmap( bitmap )
 
         username.addTextChangedListener {
             enableBtn(areFieldsFilled())
