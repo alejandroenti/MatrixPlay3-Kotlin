66e5c54288cccc4fbe99a6d173ceb13d528c6f92
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-11-10 20:26:02 +0100

    Making animation on playername2
    Expected to be blank

--- MatrixPlay/app/src/main/java/com/matrixplay3/matrixplay/activites/WaitActivity.kt
+++ MatrixPlay/app/src/main/java/com/matrixplay3/matrixplay/activites/WaitActivity.kt
@@ -3,6 +3,7 @@ package com.matrixplay3.matrixplay.activites
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
@@ -10,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.matrixplay3.matrixplay.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -19,6 +21,12 @@ class WaitActivity : BaseActivity() {
     private lateinit var titleShadow : TextView
     private lateinit var versus : TextView
     private lateinit var versusShadow : TextView
+    private lateinit var player1Image : ImageView
+    private lateinit var player2Image : ImageView
+    private lateinit var player1Name : TextView
+    private lateinit var player2Name : TextView
+
+    private var player2NambeJob: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,17 +42,35 @@ class WaitActivity : BaseActivity() {
         titleShadow = findViewById<TextView>(R.id.waitTitleShadow)
         versus = findViewById<TextView>(R.id.waitVersusTitle)
         versusShadow = findViewById<TextView>(R.id.waitVersusShadow)
+        player1Image = findViewById<ImageView>(R.id.waitImagePlayer1)
+        player2Image = findViewById<ImageView>(R.id.waitImagePlayer2)
+        player1Name = findViewById<TextView>(R.id.waitNamePlayer1)
+        player2Name = findViewById<TextView>(R.id.waitNamePlayer2)
 
         startDotAnimation(title)
         startDotAnimation(titleShadow)
         startRespirationAnimation(versus)
         startRespirationAnimation(versusShadow)
+        startDotAnimation(player2Name)
     }
 
     fun startDotAnimation(tv : TextView) {
         val baseText = getString(R.string.wait_title)
         val maxDots = 3
         val delayMillis : Long = 750
+
+        if (tv.id.equals(R.id.waitNamePlayer2)) {
+            player2NambeJob = CoroutineScope(Dispatchers.Main).launch {
+                var dotCount = 0
+                while (true) {
+                    val dots = ".".repeat(dotCount)
+                    tv.text = baseText + dots
+                    dotCount++
+                    if (dotCount > maxDots) dotCount = 0 // reinicia los puntos
+                    delay(delayMillis)
+                }
+            }
+        }
 
         CoroutineScope(Dispatchers.Main).launch {
             var dotCount = 0
